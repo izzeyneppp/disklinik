2e373e35b6d30fa666ae16f30f3bb4c9132b8db8
izzeyneppp <zeynepgengec2610@icloud.com>
2024-12-12 15:30:23 +0300

    .

--- disklinik/Program.cs
+++ disklinik/Program.cs
@@ -16,7 +16,7 @@ namespace disklinik
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new doktorkayit());
         }
     }
 }
